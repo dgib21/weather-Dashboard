27fae1f6b503c6c871d810c7964f711912a1822e
Devin Gibson <dgib21@hotmail.com>
2020-09-06 07:23:13 -0500

    added values to the current city box - even UV Index

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,11 @@
 // created variables to store refrences to the form elements 
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var temperatureEl = document.querySelector("#temp");
+var windspeedEl = document.querySelector("#windspeed"); 
+var humidityEl = document.querySelector("#humidity"); 
+var currentUVIEl = document.querySelector("#uvindex");
+
 
 
 //checking to see if the user has hit the form submission button 
@@ -31,16 +36,68 @@ var getWeatherData = function (city) {
       // request was successful
       if (response.ok) {
         response.json().then(function(data) {
-            console.log("Button was clicked")
+            displayWeatherInfo(data, city); 
         });
       } else {
-        alert("Error: City" + response.statusText);
+          // not a real city 
+        alert("Error: City " + response.statusText);
       }
     })
     .catch(function(error) {
-      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      // api is down error 
       alert("Unable to get weather data");
     });
 };
 
+var displayUvIndex = function (weatherData) {
+
+    //storing current Cityies UV Index and using the lat and lon to get the index 
+    var currentLatitude = weatherData.coord.lat; 
+    var currentLongitude = weatherData.coord.lon; 
+
+    var uvApiUrl =
+    "https://api.openweathermap.org/data/2.5/uvi?appid=2b92c62c9685a63968aa0914485ee11e&lat=" + currentLatitude +
+    "&lon=" + currentLongitude;    
+    
+    console.log (uvApiUrl); 
+
+    fetch(uvApiUrl)
+    .then(function (response) {
+        
+        return response.json();
+      })
+      .then(function (response) {
+        var currentUvIndex = response.value;
+        currentUVIEl.textContent = currentUvIndex;
+
+});
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+
+var displayWeatherInfo = function (weatherData, searchTerm) {
+    console.log(weatherData); 
+    console.log(searchTerm); 
+
+    var currentTemp = Math.round((weatherData.main.temp-273.15) * 1.8 + 32);
+   // formula found - https://www.inchcalculator.com/convert/meter-per-second-to-mile-per-hour/
+    var currentWindSpeed = Math.round(2.236936*weatherData.wind.speed);
+    var currentHumidity = weatherData.main.humidity; 
+   // var UV Index = 
+
+    
+   //displaying current values 
+    var cityTemperature = currentTemp; 
+    temperatureEl.textContent = cityTemperature; 
+
+    var cityWindSpeed = currentWindSpeed; 
+    windspeedEl.textContent = cityWindSpeed; 
+    
+    var cityHumidity = currentHumidity; 
+    humidityEl.textContent = currentHumidity;
+
+    //display that city's uv index
+    displayUvIndex(weatherData);
+    
+
+}
